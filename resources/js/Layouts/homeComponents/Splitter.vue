e962e944a0942b13d23b1f999ede48e7d7dcb7c1
leitobbUwU <121040159@upq.edu.mx>
2023-09-20 12:35:04 -0600

    Splitter debajo del tour cardenal
    se agregaron cuatro imagenes para la seccion de videos y banners debajo del boton de tour cardenal, simulando las del diseño autorizado.

---
+++ resources/js/Layouts/homeComponents/Splitter.vue
@@ -0,0 +1,9 @@
+<script setup>
+import banner_uno from '@/Components/banner_uno.vue';
+import banner_tresVue from '@/Components/banner_tres.vue';
+</script>
+
+<template>
+    <banner_tresVue></banner_tresVue>
+    <banner_uno></banner_uno>
+</template>

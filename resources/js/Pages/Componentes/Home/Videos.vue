039d5815358e5bfc25dda22a388d8d4b8676d175
BritanyOlguin <121037879@upq.edu.mx>
2023-10-01 00:46:52 -0600

    Error en consola
    Error en el AppLayout

--- resources/js/Pages/Componentes/Home/Videos.vue
+++ resources/js/Pages/Componentes/Home/Videos.vue
@@ -1,26 +1,26 @@
 <template>
-    <AppLayout title="Video" >
-        <TabView ref="tabview2">
-            <TabPanel header="Español">
-                <video src="Videos/video1.mp4" controls poster="../Img/IMGUPQ.png" class="responsive-video"/>
-            </TabPanel>
-            <TabPanel header="Ingles">
-                <video src="Videos/video2.mp4" controls poster="../Img/IMGUPQ.png" class="responsive-video"/>
-            </TabPanel>
-            <TabPanel header="Frances">
-                <video src="Videos/video1.mp4" controls poster="../Img/IMGUPQ.png" class="responsive-video"/>
-            </TabPanel>
-            <TabPanel header="Portugues">
-                <video src="Videos/video2.mp4" controls poster="../Img/IMGUPQ.png" class="responsive-video"/>
-            </TabPanel>
-        </TabView>
-    </AppLayout>
+    <TabView ref="tabview2">
+        <TabPanel header="Español">
+            <video src="Videos/video1.mp4" controls poster="../Img/IMGUPQ.png" class="responsive-video" />
+        </TabPanel>
+        <TabPanel header="Ingles">
+            <video src="Videos/video2.mp4" controls poster="../Img/IMGUPQ.png" class="responsive-video" />
+        </TabPanel>
+        <TabPanel header="Frances">
+            <video src="Videos/video1.mp4" controls poster="../Img/IMGUPQ.png" class="responsive-video" />
+        </TabPanel>
+        <TabPanel header="Portugues">
+            <video src="Videos/video2.mp4" controls poster="../Img/IMGUPQ.png" class="responsive-video" />
+        </TabPanel>
+    </TabView>
 </template>
 
 <style scoped>
 .responsive-video {
-    width: 100%; /* Ocupa todo el ancho del contenedor */
-    height: 100%; /* Se ajusta automáticamente según el aspecto del video */
-    max-height: 100%; /* No excede la altura del contenedor */
-}
-</style>
+    width: 100%;
+    /* Ocupa todo el ancho del contenedor */
+    height: 100%;
+    /* Se ajusta automáticamente según el aspecto del video */
+    max-height: 100%;
+    /* No excede la altura del contenedor */
+}</style>

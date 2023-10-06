05c15febc331b6365a66260991b9c00d626f020f
DiegoMP-235 <121039161@upq.edu.mx>
2023-10-05 20:29:19 -0600

    Se agrego la pantalla que refiere a la carrera

---
+++ resources/js/carreras/Carreras.vue
@@ -0,0 +1,41 @@
+<template>
+    <section class="carreras">
+        <h1>Carrers components</h1>
+    </section>
+</template>
+
+<script>
+import { defineComponent } from 'vue';
+import PerfilesCarrera from '@/carreras/perfiles/PerfilesCarrera.vue';
+import CiclosFormacion from '@/carreras/ciclos_formacion/CiclosFormacion.vue';
+
+export default defineComponent({
+    name:'Carreras',
+    components:{
+      PerfilesCarrera,
+      CiclosFormacion,
+    },
+  // Propiedades del componente (opcional)
+  props: {
+
+  },
+  // Setup del componente (opcional)
+  setup(props) {
+
+    return {
+
+    };
+  },
+
+
+});
+</script>
+
+<style scoped>
+/*
+* {
+  margin:0;
+  padding:0;
+}
+*/
+</style>

a9f9f49a9e0b95cc586194bbf17f30f761a7461c
DiegoMP-235 <121039161@upq.edu.mx>
2023-11-04 22:43:00 -0600

    Se cambio el tamaño del contenedor para visualizar la informacion relacionada

--- resources/js/Pages/carreras/Components/CarrerasList.vue
+++ resources/js/Pages/carreras/Components/CarrerasList.vue
@@ -40,7 +40,7 @@
 
         <!--dialogo para mostrar los datos de la carrera a eliminar-->
         <dialog-pv v-model:visible="visibleDialogDelete" :breakpoits="{ '960px': '75vw', '640px': '85vw' }" 
-        :style="{ width: '25vw' }" header="Ver datos relacionados" modal class="p-fluid">
+        :style="{ width: '70vw' }" header="Ver datos relacionados" modal class="p-fluid">
             <div class="field">
                 <form @submit.prevent="">
                     <div class="container-datos-relacionados-eliminar">
@@ -96,7 +96,7 @@
                             <div class="titulo-apartado centrar">
                                 <h5>Perfil ingreso</h5>
                             </div>
-                            
+
                             <div class="container-conocimientos">
 
                             </div>

1d859baf57fd9fe942d10a937918fa7d84e8dc0e
adrianchino15 <adrian_w15@outlook.com>
2023-11-09 14:38:40 -0600

    carrusel repre

--- resources/js/Pages/Componentes/FormacionIntegral/representativos.vue
+++ resources/js/Pages/Componentes/FormacionIntegral/representativos.vue
@@ -1,5 +1,6 @@
 <script>
 import AppEstructure from '@/Layouts/mainEstructure/AppEstructure.vue';
+import Carousel from 'primevue/carousel';
 
 export default {
     components: {
@@ -29,6 +30,28 @@ export default {
         return {
             texto: [],
             deporte: [],
+            responsiveOptions: [
+                {
+                    breakpoint: '1400px',
+                    numVisible: 2,
+                    numScroll: 1
+                },
+                {
+                    breakpoint: '1199px',
+                    numVisible: 3,
+                    numScroll: 1
+                },
+                {
+                    breakpoint: '767px',
+                    numVisible: 2,
+                    numScroll: 1
+                },
+                {
+                    breakpoint: '575px',
+                    numVisible: 1,
+                    numScroll: 1
+                }
+            ]   
         };
     },
 }
@@ -40,9 +63,6 @@ export default {
 <template>
     <AppEstructure :controllerName="'/Representativosprin/bannerData'">
         {{ deporte }}
-        <div v-for="depo in deporte">
-        {{ depo.nombre }}
-        </div>
         <div class="h-3rem" style="background-color:rgb(140, 36, 55);"></div>
         <div class="panel">
             <div class="texto px-8" v-for="text in texto">
@@ -55,13 +75,31 @@ export default {
             </div>
         </div>
 
+
+        <div class="card">
+        <Carousel :value="deporte" :numVisible="3" :numScroll="1" :responsiveOptions="responsiveOptions" circular :autoplayInterval="3000">
+            <template #item="slotProps">
+                <div class="border-1 surface-border border-round m-2 text-center py-5 px-3">
+                    <div class="mb-3">
+                        <img :src="'/storage/' + slotProps.data.imagen" alt="Card Image" class="w-11 h-auto" />
+                    </div>
+                    <div>
+                        <h4 class="mb-1">{{ slotProps.data.nombre }}</h4>
+                        
+                    </div>
+                </div>
+            </template>
+        </Carousel>
+    </div>
+
+
         <div class="flex align-items-center justify-content-center">
             <div class="mt-8 border-bottom-3 w-11 text-right">
                 <h2>Difusión Cultural y Artistica (DICART)</h2>
             </div>
         </div>
 
-        
+
     </AppEstructure>
 </template>
 
@@ -75,7 +113,7 @@ export default {
 .texto {
     padding: 50px;
     text-align: center;
-    font-size: 1.5rem; 
+    font-size: 1.5rem;
 }
 </style>
 

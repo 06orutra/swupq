bcbd187eaf1390214c4eb0eba8a3eba421549a59
adrianchino15 <adrian_w15@outlook.com>
2023-11-10 12:12:30 -0600

    cambio de imagen tour cardenal

--- resources/js/Layouts/homeComponents/TourCardenal.vue
+++ resources/js/Layouts/homeComponents/TourCardenal.vue
@@ -2,7 +2,7 @@
         <div class="generic">
                 <a href="https://es.wikipedia.org/wiki/Revoluci%C3%B3n_mexicana" target="_blank" class="link-reset">
                         <img class="w-full md:h-32 lg:h-64 img-reset"
-                                src="/storage/img/TourCardenal.png" alt="altText" />
+                                src="/storage/img/TourCardenal.svg" alt="altText" />
                 </a>
         </div>
 </template>
